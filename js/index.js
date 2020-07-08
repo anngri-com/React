3c7a410af3ce8a235f065e20461e0e02e61c3bea
anngri-com <anna.gritsay@gmail.com>
2020-07-08 15:59:18 -0700

    now added some react code but still doesn't work

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+ReactDom.render(
+    <ul>
+        <li>one</li>
+        <li>one</li>
+        <li>one</li>
+    </ul>,
+    document.getElementById("root")
+)
